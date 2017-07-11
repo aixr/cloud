8a338f958c3249b09e6d5b34fc926fde3c799f2e
Chris <aixrjnr@gmail.com>
2017-07-11 15:17:34 -0400

    made fixes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt-get -y install mininet
-sudo apt-get -y python-pip python-dev
+sudo apt-get -y install python-pip
+sudo apt-get -y install python-dev
 sudo apt-get -y install python-eventlet python-routes python-webob python-paramiko python-matplotlib
 
 git clone --depth=1 https://github.com/osrg/ryu.git
@@ -11,8 +12,10 @@ sudo python setup.py install
 
 sudo pip install six --upgrade
 sudo pip install tinyrpc --upgrade
-sudo pip install oslo.config msgpack-pyhton
+sudo pip install oslo.config msgpack-python
 sudo pip install eventlet --upgrade
+sudo pip install ovs --upgrade
 
 ryu-manager --version
 
+sudo apt-get -y install php
